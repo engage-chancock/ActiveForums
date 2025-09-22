d624be334292fad3f74f27c5bb9764028b803a96
Cregg Hancock <chancock@engagesoftware.com>
2025-09-22 16:56:31 -0500

    Replace usage of deprecated GetModuleSettings

--- CustomControls/UserControls/WhatsNewRSS.cs
+++ CustomControls/UserControls/WhatsNewRSS.cs
@@ -115,7 +115,7 @@ namespace DotNetNuke.Modules.ActiveForums.Controls
                     var moduleSettings = DataCache.CacheRetrieve(settingsCacheKey) as Hashtable;
                     if (moduleSettings == null)
                     {
-                        moduleSettings = new ModuleController().GetModuleSettings(RequestModuleID);
+                        moduleSettings = new DotNetNuke.Entities.Modules.ModuleController().GetModule(moduleID: RequestModuleID).ModuleSettings;
                         DataCache.CacheStore(settingsCacheKey, moduleSettings);
                     }
 
